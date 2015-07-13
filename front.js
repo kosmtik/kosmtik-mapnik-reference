5515c8a04c06013d858dbf2747379e316bfbc93b
Yohan Boniface <yb@enix.org>
2015-07-13 17:31:00 +0200

    0.0.4
    - display range property (fix kosmtik/kosmtik#68)
    - display rule status (fix kosmtik/kosmtik#67)
    - simplify filter with only one input
    - focus on filter input on load

--- front.js
+++ front.js
@@ -2,25 +2,28 @@ L.K.Map.addInitHook(function () {
     this.whenReady(function () {
         var container = L.DomUtil.create('div', 'mapnik-reference-container'), data,
             title = L.DomUtil.create('h3', '', container),
-            formContainer = L.DomUtil.create('div', '', container),
             docContainer = L.DomUtil.create('div', '', container),
             params = {
                 filterSymbolizers: '',
                 filterRules: ''
             },
-            builder = new L.K.FormBuilder(params, [
-                ['filterSymbolizers', {handler: 'Input', placeholder: 'Filter symbolizers…'}],
-                ['filterRules', {handler: 'Input', placeholder: 'Filter rules…'}]
+            form = new L.K.FormBuilder(params, [
+                ['filter', {handler: 'Input', placeholder: 'Filter symbolizers and rules…'}]
             ], {id: 'mapnik-reference-form'});
-        formContainer.appendChild(builder.build());
-        var addRule = function (container, name, properties) {
+        title.innerHTML = 'CartoCSS Reference (<span class="version"></span>)';
+        title.appendChild(form.build());
+        var addRule = function (container, parentName, name, properties) {
             name = properties.css || name;
-            var filter = (params.filterRules || '').toLowerCase();
-            if (filter && name.toLowerCase().indexOf(filter) === -1) return;
+            var filter = (params.filter || '').toLowerCase();
+            if ((parentName + ' ' + name).search(filter.split('').join('.*')) === -1) return;
             var title = L.DomUtil.create('h5', 'rule', container);
             title.innerHTML = name + '=';
             var type = L.DomUtil.create('em', 'type', title);
             type.innerHTML = L.Util.isArray(properties.type) ? 'list' : properties.type;
+            if (properties.status && properties.status !== 'stable') {
+                var status = L.DomUtil.create('span', 'status ' + properties.status, title);
+                status.innerHTML = properties.status;
+            }
             var doc = L.DomUtil.create('p', 'doc', container);
             doc.innerHTML = properties.doc;
             var el = L.DomUtil.create('p', '', container);
@@ -37,23 +40,30 @@ L.K.Map.addInitHook(function () {
                 el = L.DomUtil.create('p', '', container);
                 el.innerHTML = '<strong>Functions: </strong>' + properties.functions.join(', ');
             }
+            if (properties.range) {
+                el = L.DomUtil.create('p', '', container);
+                el.innerHTML = '<strong>Range: </strong>' + properties.range;
+            }
         };
         var addSymbolizer = function (name, rules) {
-            var filter = (params.filterSymbolizers || '').toLowerCase();
-            if (filter && name.toLowerCase().indexOf(filter) === -1) return;
             var wrapper = L.DomUtil.create('div', 'symbolizer', docContainer);
             var title = L.DomUtil.create('h4', '', wrapper);
             var content = L.DomUtil.create('div', 'rules', wrapper);
             title.innerHTML = name;
-            for (var rule in rules) addRule(content, rule, rules[rule]);
+            for (var rule in rules) addRule(content, name, rule, rules[rule]);
+            // No rule has matched: hide.
+            if (!content.textContent) wrapper.style.display = 'none';
+            else wrapper.style.display = 'block';
         };
         var build = function () {
-            title.innerHTML = 'CartoCSS Reference (' + data.version + ')';
+            var versionContainer = container.querySelector('.version');
+            versionContainer.textContent = data.version;
             docContainer.innerHTML = '';
             for (var symbolizer in data.symbolizers) addSymbolizer(symbolizer, data.symbolizers[symbolizer]);
         };
-        builder.on('postsync', build);
+        form.on('postsync', build);
         var fetchData = function () {
+            form.helpers.filter.input.focus();
             if (data) return;
             L.K.Xhr.get('/mapnik-reference/', {
                 callback: function (status, _data) {

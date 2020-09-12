35a9c03475b7c0b9040b52e92b94f3d1df6ac424
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:43:03 +0200

    Use class syntax
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var ref = require('mapnik-reference');
 
-var Plugin = function (config) {
-    config.addJS('/node_modules/kosmtik-mapnik-reference/front.js');
-    config.addCSS('/node_modules/kosmtik-mapnik-reference/front.css');
-    config.on('server:init', this.attachRoutes.bind(this));
-};
+class Plugin {
+    constructor(config) {
+        config.addJS('/node_modules/kosmtik-mapnik-reference/front.js');
+        config.addCSS('/node_modules/kosmtik-mapnik-reference/front.css');
+        config.on('server:init', this.attachRoutes.bind(this));
+    }
 
-Plugin.prototype.serve = function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'application/javascript'
-    });
-    res.write(JSON.stringify(ref.load(this.config.parsed_opts.mapnik_version)));
-    res.end();
-};
+    serve(req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'application/javascript'
+        });
+        res.write(JSON.stringify(ref.load(this.config.parsed_opts.mapnik_version)));
+        res.end();
+    }
 
-Plugin.prototype.attachRoutes = function (e) {
-    e.server.addRoute('/mapnik-reference/', this.serve);
-};
+    attachRoutes(e) {
+        e.server.addRoute('/mapnik-reference/', this.serve);
+    }
+}
 
-exports.Plugin = Plugin;
+exports = module.exports = { Plugin };
